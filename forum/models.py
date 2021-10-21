f2753d53768bd90bdacb4e46d7bf990503aa9218
Lorenzo Lerate <lorenzolerate@gmail.com>
2021-10-21 12:53:33 +0200

    Added views

--- forum/models.py
+++ forum/models.py
@@ -1,14 +1,16 @@
+from typing_extensions import Required
 from django.db import models
 
 class Author(models.Model):
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
+    email = models.EmailField(required=True)
     join_date = models.DateTimeField('join date')
     last_login_date = models.DateTimeField()
-    avatar = models.ImageField(upload_to='avatars')
+    avatar = models.ImageField(upload_to='avatar')
 
     def __str__(self):
-        return self.username    
+        return self.username
 
 class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.PROTECT)
@@ -21,8 +23,9 @@ class Post(models.Model):
         return self.post_title  
 
 class Response(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    reponse_text = models.TextField(max_length=200)
+    response_text = models.TextField()
     pub_date = models.DateTimeField('date published')
     votes = models.IntegerField(default=0)
 
