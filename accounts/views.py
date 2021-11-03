c5928718eb581bad65f07f6505d3dc00f0f22405
Lorenzo Lerate <lorenzolerate@gmail.com>
2021-11-03 07:09:46 +0100

    Add auto login after registration and return to main or previous page

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,15 @@
+from django.http.response import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import get_object_or_404, render
 from accounts.forms import SignUpForm
 from django.core.exceptions import PermissionDenied
+from django.contrib.auth import authenticate, login
 
 from forum.models import User
 
 
+ROOT_URL = '/'
 
 PROFILE_INFO_TEMPLATE = "registration/user_detail.html"
 SIGN_UP_TEMPLATE = 'registration/sign_up.html'
@@ -14,8 +17,17 @@ SIGN_UP_TEMPLATE = 'registration/sign_up.html'
 
 class SignUpView(generic.CreateView):
     form_class = SignUpForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('index')
     template_name = SIGN_UP_TEMPLATE
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        super().form_valid(form)
+        user = authenticate(
+            username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+        login(self.request, user)
+        return HttpResponseRedirect(ROOT_URL)
 
 
 def user_detail(request, user_id):
@@ -40,5 +52,5 @@ def logged_user_detail(request):
             'user': get_object_or_404(User, id=request.user.id),
         }
         return render(request, PROFILE_INFO_TEMPLATE, context)
-    else:    
+    else:
         raise PermissionDenied()
