3044d1857b206d74b1bda767edbc83b8f018967c
Lorenzo Lerate <lorenzolerate@gmail.com>
2021-11-03 22:28:08 +0100

    Add dates in local timezone

--- forum/admin.py
+++ forum/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
-from .models import User, Discussion, Response, Vote
+from .models import Message, User, Discussion, Response, Vote
 
 admin.site.register(User, ModelAdmin)
 admin.site.register(Discussion)
 admin.site.register(Response)
+admin.site.register(Message)
 admin.site.register(Vote)
