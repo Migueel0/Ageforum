b942d4ef947d3c2f8e5750389482efeb66e9ec7a
Migueel0 <miggaller@alum.us.es>
2021-11-06 03:35:44 +0100

    simple details

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from accounts import urls
+
 from . import views
 
 
@@ -7,7 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('discussion_create', views.discussion_create, name='discussion_create'),
     path('response_create/<int:discussion_id>', views.response_create, name='response_create'),
-    path('response_reply_create/<int:discussion_id>/<int:message_id>', views.response_reply_create, name='response_reply_create'),
+    path('response_eply_create/<int:discussion_id>/<int:message_id>', views.response_reply_create, name='response_reply_create'),
     path('<int:discussion_id>', views.discussion_detail, name='discussion_detail'),
     path('message_vote/<int:message_id>', views.message_vote, name='message_vote'),
 ]
