630e6e1827a3eef9e1ed06707704a532589ab93e
Lorenzo Lerate <lorenzolerate@gmail.com>
2021-11-10 17:34:10 +0100

    Add route to robots.txt

--- forum/urls.py
+++ forum/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from accounts import urls
 
@@ -8,8 +9,13 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('discussion_create', views.discussion_create, name='discussion_create'),
-    path('response_create/<int:discussion_id>', views.response_create, name='response_create'),
-    path('response_reply_create/<int:discussion_id>/<int:message_id>', views.response_reply_create, name='response_reply_create'),
+    path('response_create/<int:discussion_id>',
+         views.response_create, name='response_create'),
+    path('response_reply_create/<int:discussion_id>/<int:message_id>',
+         views.response_reply_create, name='response_reply_create'),
     path('<int:discussion_id>', views.discussion_detail, name='discussion_detail'),
-    path('message_vote/<int:message_id>', views.message_vote, name='message_vote'),
+    path('message_vote/<int:message_id>',
+         views.message_vote, name='message_vote'),
+    path("robots.txt", TemplateView.as_view(
+        template_name="robots.txt", content_type="text/plain")),
 ]
